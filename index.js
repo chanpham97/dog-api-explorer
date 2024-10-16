45fd5e01dd899de2d0d862568afa69512368e65b
Chan Pham <iamchanpham@gmail.com>
2024-10-16 22:54:01 +0000

    group task template

---
+++ index.js
@@ -0,0 +1,20 @@
+// Task 1
+// Go to the Dog CEO API (from HW 2) and take a look at the documentation
+// Define a variable called requestUrl and put in your team's requestUrl
+
+
+// Task 2
+// Define a function that fetches data from your requestUrl
+// In the function, start by console logging the entire data JSON
+
+
+
+
+// Task 3
+// Call the function and make sure you see the data JSON in your console
+
+
+
+// Task 4
+// Based on the data JSON, modify what you console logged in the function to get your team's specific data
+// This task doesn't need extra code! Just go back to task 2 and modify it.
